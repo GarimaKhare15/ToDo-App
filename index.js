f4f2b4670ef04298130adac7044236b56565ae75
GarimaKhare15 <garimakhare.cse19@chitkarauniversity.edu.in>
2022-02-09 22:30:08 +0530

    added add task, delete task functality

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const PORT = 4000;
-const express = require('express');
+const express = require("express");
 const app = express();
-const Task = require('./models/taskList');
-const database = require('./config/mongoose');
+const Task = require("./models/taskList");
+const database = require("./config/mongoose");
 //setting ejs as view engine
-app.set('view engine', 'ejs');
-app.set('views','./views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //middleware
 
@@ -23,37 +23,52 @@ app.set('views','./views');
 ]
 */
 
-app.use(express.urlencoded({ extended:true}));
-app.use(express.static('public'));
-app.get('/', function(req, res){
-    Task.find({}, function(err,task){
-                 if(err){
-                     console.log("Error tasks")
-                    return;
-                }
-                 return res.render('app',{Tasks : task})
-             })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  Task.find({}, function (err, task) {
+    if (err) {
+      console.log("Error tasks");
+      return;
+    }
+    return res.render("app", { Tasks: task });
+  });
 });
 
-app.post('/addTask',function(req,res){
-    Task.create({
-        description: req.body.description,
-        dueDate: req.body.dueDate,
-        category: req.body.category
-    },function(err,newTask){
-        if(err){
-            console.log('Error in adding task: ' + err.message);
-            return;
-        }
-        console.log(req.body);
-        return res.redirect('back');
-    })
+app.post("/addTask", function (req, res) {
+  Task.create(
+    {
+      description: req.body.description,
+      dueDate: req.body.dueDate,
+      category: req.body.category,
+    },
+    function (err, newTask) {
+      if (err) {
+        console.log("Error in adding task: " + err.message);
+        return;
+      }
+      console.log(req.body);
+      return res.redirect("back");
+    }
+  );
+});
+
+app.get('/deleteTask',function(req, res) {
+
+  Task.findByIdAndDelete(req.query.id, function(err){
+    if(err){
+      console.log("Error in deleting task: " + err.message);
+      return;
+    }
+    return res.redirect("back");
+  })
 })
 
-app.listen(PORT, function(err) {
-    if(err){
-        console.log(err);
-        return;
+  app.listen(PORT, function (err) {
+    if (err) {
+      console.log(err);
+      return;
     }
-    console.log('listening on port', PORT);
-})
+    console.log("listening on port", PORT);
+  })
+
